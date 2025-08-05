3951d291135584ffa59d73fab7973e33de84006c
Yuan Chiu <chyuaner@gmail.com>
2025-08-05 17:34:42 +0800

    feat: ui改良，並新增完成後不關閉本視窗功能

--- src/gui/MainWindow.py
+++ src/gui/MainWindow.py
@@ -18,13 +18,22 @@ class MainWindow(QMainWindow):
     def showInfo(self):
         self.ui.apiurl_label.setText(config.mcapiserver_url)
         self.ui.prefix_label.setText(config.prefix)
-        self.ui.modsFolder_label.setText(str(config.mods_path))
+        self.ui.modsFolder_label.setText(
+            '<html><head/><body><p>'
+            +'<a href="file://'+str(config.mods_path)+'"><span style=" text-decoration: underline; color:#2980b9;">'+str(config.mods_path)
+            +'</span></a></p></body></html>'
+            )
 
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)  # 將 UI 佈局設到 self (QMainWindow)
         self.showInfo()
+        self.sync_thread = None
+
+        # 隱藏目前暫無功能的Layout
+        self.ui.extra_widget.setVisible(False)
+        self.ui.step_widget.setVisible(False)
 
         # 建立 stream 並連接
         self.output_stream = EmittingStream()
@@ -40,15 +49,18 @@ class MainWindow(QMainWindow):
         # 你的事件連接與邏輯在這寫
         self.ui.skip_pBtn.clicked.connect(self.skipClick)
         self.ui.cancel_pBtn.clicked.connect(self.cancelClick)
-        # 
+        self.ui.continue_pBtn.clicked.connect(self.skipClick)
+        self.ui.exit_pBtn.clicked.connect(self.cancelClick)
+        
+        self.ui.finished_btnGroup.setVisible(False)
 
     def append_text(self, text):
         self.ui.output_textBrowser.moveCursor(QTextCursor.End)
         self.ui.output_textBrowser.insertPlainText(text)
-
+        
     def skipClick(self):
         self.isSkip = True
-        self.isFinal = False
+        # self.isFinal = False
         self.close()
 
     def cancelClick(self):
@@ -57,7 +69,8 @@ class MainWindow(QMainWindow):
         self.close()
 
     def closeEvent(self, event):
-        if hasattr(self, "sync_thread") and self.sync_thread.isRunning():
+        self.exit_code = 0
+        if self.sync_thread and self.sync_thread.isRunning():
             self.sync_thread.requestInterruption()
             # 等 thread 結束，阻塞 UI 不太好，可用非阻塞提示用戶
             self.sync_thread.finished.connect(lambda: self.close())  # 結束後再關閉
@@ -83,7 +96,8 @@ class MainWindow(QMainWindow):
         self.worker.finished.connect(self.sync_thread.quit)
         self.worker.finished.connect(self.worker.deleteLater)
         self.sync_thread.finished.connect(self.sync_thread.deleteLater)
-
+        self.sync_thread.finished.connect(self.clear_sync_thread)
+        
         # self.startBtn.setEnabled(False)
         # self.progressBar.setValue(0)
         self.on_start()
@@ -91,7 +105,6 @@ class MainWindow(QMainWindow):
 
     def on_start(self):
         self.ui.total_progressBar.setValue(0)
-
 
     def on_progress(self, info: dict):
         self.ui.total_progressBar.setValue(int(info["total"] * 100))
@@ -109,5 +122,14 @@ class MainWindow(QMainWindow):
     def on_finished(self, success: bool):
         print("同步完成" if success else "同步被中斷")
         self.isFinal = True if success else False
-        self.close()
+
+        self.ui.process_btnGroup.setVisible(False)
+        self.ui.finished_btnGroup.setVisible(True)
+
+    
+    def clear_sync_thread(self):
+        self.sync_thread = None
+        if self.ui.finishNoClose_checkBox.isChecked() is not True:
+            self.close()
+
 
